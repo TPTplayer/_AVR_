ccd3d85f9ca5b3da1ea4159b5607762948280606
Kenujae Jung <potalgun0921@naver.com>
2019-06-25 08:44:25 +0900

    Update: 2019.06.25

---
+++ function/TWI_master_transmit/master/TWI_master.h
@@ -0,0 +1,11 @@
+﻿#ifndef TWI_H_
+#define TWI_H_
+
+void TWI_initializer(void);
+void TWI_start(void);
+void TWI_transmit(uint8_t slave_addr, uint8_t data);
+void TWI_stop(void);
+
+#define SLAVE_ADDR 0x23
+
+#endif /* TWI_H_ */
---
+++ function/TWI_master_transmit/slave/TWI_slave.c
@@ -0,0 +1,24 @@
+﻿#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <util/twi.h>
+#include <stdint.h>
+
+#include "TWI_slave.h"
+
+void TWI_initializer(void){
+	cli();
+	TWAR = (SLAVE_ADDR << 1) & 0xFE;
+	TWCR = (1 << TWIE) | (1 << TWEA) | (1 << TWINT) | (1 << TWEN);
+	sei();
+}
+
+ISR(TWI_vect){
+	if(TW_STATUS == TW_SR_DATA_ACK){
+		
+	}
+	else if(TW_STATUS == TW_BUS_ERROR){
+		TWCR = 0x00;
+		TWCR = (1 << TWIE) | (1 << TWEA) | (1 << TWINT) | (1 << TWEN);
+	}
+}
