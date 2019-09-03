d3fcc547f1cea6bb7a29ceb1e6aec066d2534a73
Kenujae Jung <potalgun0921@naver.com>
2019-09-04 08:52:12 +0900

    HC-SR04 driver

---
+++ drivers/ultrasonic_sensor_HC_SR04/ultrasonic_sensor.h
@@ -0,0 +1,17 @@
+﻿#ifndef ULTRASONIC_SENSOR_H_
+#define ULTRASONIC_SENSOR_H_
+
+#define TRIG 0x01
+#define TRIG_PORT PORTA
+
+void timer_start(void);
+void timer_end(void);
+void falling_edge_EINT_set(void);
+void trig_set(void);
+void start_echo_measurement(void);
+void end_echo_measurement(void);
+float distance_calculation(int duration);
+
+float ultrasonic_sensor(void);
+
+#endif /* ULTRASONIC_SENSOR_H_ */
