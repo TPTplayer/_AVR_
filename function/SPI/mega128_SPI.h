b3560624a717b4cfc39801c5d8753d208c79c157
Kenujae Jung <potalgun0921@naver.com>
2020-07-29 16:02:04 +0900

    update: 2020.07.29

---
+++ function/SPI/mega128_SPI.h
@@ -0,0 +1,25 @@
+﻿#ifndef MEGA128_SPI_H_
+#define MEGA128_SPI_H_
+
+#define DDR_SPI DDRB
+#define PORT_SPI PORTB
+
+#define SS 0
+#define SCK 1
+#define MOSI 2
+#define MISO 3
+
+void SPI_master_initializer(void);
+void SPI_slave_initializer(void);
+uint8_t SPI_master_com(uint8_t data);
+uint8_t SPI_slave_com(uint8_t data);
+
+/*
+<connection>
+SS(CS) - SS(CS)
+SCK - SCK
+MOSI - MOSI
+MISO - MISO
+*/
+
+#endif /* MEGA128_SPI_H_ */
