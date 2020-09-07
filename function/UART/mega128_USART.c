dd808d955d26b53e2af73e6fbd2ca921d148db8e
TPTplayer <potalgun0921@naver.com>
2020-09-07 16:59:21 +0900

    update: 2020.09.07

--- function/UART/mega128_USART.c
+++ function/UART/mega128_USART.c
@@ -30,7 +30,7 @@ void UART_initializer(int uart_num, uint32_t ubrr){
 	return;	
 }
 
-void UART_transmit(int uart_num, uint8_t data){
+void __UART_transmit(int uart_num, uint8_t data){
 	switch(uart_num){
 	case 0:
 		while(!(UCSR0A & (1 << UDRE0)));
@@ -45,7 +45,7 @@ void UART_transmit(int uart_num, uint8_t data){
 	return;
 }
 
-uint8_t UART_receive(int uart_num){
+uint8_t __UART_receive(int uart_num){
 	uint8_t data = 0;
 	
 	switch(uart_num){
@@ -79,15 +79,48 @@ void UART_flush(int uart_num){
 	}
 }
 
-int UART_transmit_stdout(char data, FILE *stream){
+int __UART_transmit_stdout_num0(char data, FILE *stream){
+	while(!(UCSR0A & (1 << UDRE0)));
+	UDR0 = data;
+	
+	return 0;
+}
+
+int __UART_transmit_stdout_num1(char data, FILE *stream){
 	while(!(UCSR1A & (1 << UDRE1)));
 	UDR1 = data;
 	
 	return 0;
 }
 
-void setup_stdout(FILE *stream){
-	stream = fdevopen(UART_transmit_stdout, NULL);
+int __UART_receive_stdin_num0(FILE *stream){
+	int data = 0;
+	
+	while(!(UCSR0A & (1 << RXC0)));
+	data = UDR0;
+	
+	return data;
+}
+
+int __UART_receive_stdin_num1(FILE *stream){
+	int data = 0;
+	
+	while(!(UCSR1A & (1 << RXC1)));
+	data = UDR1;
+	
+	return data;
+}
+
+void setup_stdio(int uart_num, FILE *stream){
+	switch(uart_num){
+	case 0:
+		stream = fdevopen(__UART_transmit_stdout_num0, __UART_receive_stdin_num0);
+		break;
+	case 1:
+		stream = fdevopen(__UART_transmit_stdout_num1, __UART_receive_stdin_num1);
+		break;
+	}
+	
 	return;
 }
 
--- function/UART/mega128_USART.h
+++ function/UART/mega128_USART.h
@@ -4,17 +4,20 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define USART_OSC 16000000
+#define USART_OSC 11059200
 #define BAUD 9600
 #define UBRR_VAL USART_OSC / 16 / BAUD - 1
 
 void UART_initializer(int uart_num, uint32_t ubrr);
-void UART_transmit(int uart_num, uint8_t data);
-uint8_t UART_receive(int uart_num);
+void __UART_transmit(int uart_num, uint8_t data);
+uint8_t __UART_receive(int uart_num);
 void UART_flush(int uart_num);
 
-void setup_stdout(FILE *stream);
-int UART_transmit_stdout(char data, FILE *stream);
+void setup_stdio(int uart_num, FILE *stream);
+int __UART_transmit_stdout_num0(char data, FILE *stream);
+int __UART_transmit_stdout_num1(char data, FILE *stream);
+int __UART_receive_stdin_num0(FILE *stream);
+int __UART_receive_stdin_num1(FILE *stream);
 
 /*
 <connection>
