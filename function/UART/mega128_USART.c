5e3f58239f681fe71931bde33edf173bcb381287
TPTplayer <potalgun0921@naver.com>
2020-09-07 23:32:54 +0900

    update: 2020.09.07

--- function/UART/mega128_USART.c
+++ function/UART/mega128_USART.c
@@ -5,122 +5,94 @@
 
 #include "mega128_USART.h"
 
-void UART_initializer(int uart_num, uint32_t ubrr){
-	switch(uart_num){
-	case 0:
-		UBRR0H = (uint8_t)(ubrr >> 8);
-		UBRR0L = (uint8_t)ubrr;
-		// set baudrate
-		
-		UCSR0B = (1 << RXEN0) | (1 << TXEN0); 
-		// receive/trasmit enable
-		
-		UCSR0C = (1 << USBS0) | (1 << UCSZ01) | (1 << UCSZ00); 
-		//8bit character, 2 stop bit, no parity
-		break;
-	case 1:
-		UBRR1H = (uint8_t)(ubrr >> 8);
-		UBRR1L = (uint8_t)ubrr;
-		
-		UCSR1B = (1 << RXEN1) | (1 << TXEN1);
-		UCSR1C = (1 << USBS1) | (1 << UCSZ11) | (1 << UCSZ10);
-		break;
-	}
-	
-	return;	
-}
-
-void __UART_transmit(int uart_num, uint8_t data){
-	switch(uart_num){
-	case 0:
-		while(!(UCSR0A & (1 << UDRE0)));
-		UDR0 = data;
-		break;
-	case 1:
-		while(!(UCSR1A & (1 << UDRE1)));
-		UDR1 = data;
-		break;	
-	}
+void UART0_initializer(uint32_t ubrr){
+	UBRR0H = (uint8_t)(ubrr >> 8);
+	UBRR0L = (uint8_t)ubrr;
+	// set baudrate
+			
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+	UCSR0C = (1 << USBS0) | (1 << UCSZ01) | (1 << UCSZ00);
+	//8bit data, 2 stop bit, no parity
 	
 	return;
 }
 
-uint8_t __UART_receive(int uart_num){
-	uint8_t data = 0;
-	
-	switch(uart_num){
-	case 0:
-		while(!(UCSR0A & (1 << RXC0)));
-		data = UDR0;
-		break;
-	case 1:
-		while(!(UCSR1A & (1 << RXC1)));
-		data = UDR1;
-		break;
-	}
-	
-	return data;
+void UART1_initializer(uint32_t ubrr){
+	UBRR1H = (uint8_t)(ubrr >> 8);
+	UBRR1L = (uint8_t)ubrr;
+		
+	UCSR1B = (1 << RXEN1) | (1 << TXEN1);
+	UCSR1C = (1 << USBS1) | (1 << UCSZ11) | (1 << UCSZ10);	
+
+	return;
 }
 
-void UART_flush(int uart_num){
-	uint8_t dummy;
-	
-	switch(uart_num){
-	case 0:
-		while(UCSR0A & (1 << RXC0)){
-			dummy = UDR0;
-		}
-		break;
-	case 1:
-		while(UCSR1A & (1 << RXC1)){
-			dummy = UDR1;
-		}
-		break;	
-	}
-}
-
-int __UART_transmit_stdout_num0(char data, FILE *stream){
+void __UART0_transmit(uint8_t data){
 	while(!(UCSR0A & (1 << UDRE0)));
 	UDR0 = data;
 	
-	return 0;
+	return;
 }
 
-int __UART_transmit_stdout_num1(char data, FILE *stream){
+void __UART1_transmit(uint8_t data){
 	while(!(UCSR1A & (1 << UDRE1)));
-	UDR1 = data;
+	UDR1 = data;	
 	
-	return 0;
+	return;
 }
 
-int __UART_receive_stdin_num0(FILE *stream){
-	int data = 0;
-	
+uint8_t __UART0_receive(void){
 	while(!(UCSR0A & (1 << RXC0)));
-	data = UDR0;
-	
-	return data;
+	return UDR0;	
 }
 
-int __UART_receive_stdin_num1(FILE *stream){
-	int data = 0;
-	
+uint8_t __UART1_receive(void){
 	while(!(UCSR1A & (1 << RXC1)));
-	data = UDR1;
-	
-	return data;
+	return UDR1;
 }
 
-void setup_stdio(int uart_num, FILE *stream){
-	switch(uart_num){
-	case 0:
-		stream = fdevopen(__UART_transmit_stdout_num0, __UART_receive_stdin_num0);
-		break;
-	case 1:
-		stream = fdevopen(__UART_transmit_stdout_num1, __UART_receive_stdin_num1);
-		break;
+void UART0_flush(void){
+	uint8_t dummy;
+	while(UCSR0A & (1 << RXC0)){
+		dummy = UDR0;
+	}	
+	
+	return;
+}
+
+void UART1_flush(void){
+	uint8_t dummy;
+	while(UCSR1A & (1 << RXC1)){
+		dummy = UDR1;
 	}
 	
 	return;
 }
 
+int __UART0_transmit_stdout(char data, FILE *stream){
+	__UART0_transmit((uint8_t) data);
+	return 0;
+}
+
+int __UART1_transmit_stdout(char data, FILE *stream){
+	__UART1_transmit((uint8_t) data);
+	return 0;
+}
+
+int __UART0_receive_stdin(FILE *stream){
+	return __UART0_receive();
+}
+
+int __UART1_receive_stdin(FILE *stream){
+	return __UART1_receive();
+}
+
+void UART0_setup_stdio(FILE *stream){
+	stream = fdevopen(__UART0_transmit_stdout, __UART0_receive_stdin);
+	return;
+}
+
+void UART1_setup_stdio(FILE *stream){
+	stream = fdevopen(__UART1_transmit_stdout, __UART1_receive_stdin);
+	return;
+}
--- function/UART/mega128_USART.h
+++ function/UART/mega128_USART.h
@@ -8,16 +8,24 @@
 #define BAUD 9600
 #define UBRR_VAL USART_OSC / 16 / BAUD - 1
 
-void UART_initializer(int uart_num, uint32_t ubrr);
-void __UART_transmit(int uart_num, uint8_t data);
-uint8_t __UART_receive(int uart_num);
-void UART_flush(int uart_num);
+void UART0_initializer(uint32_t ubrr);
+void UART1_initializer(uint32_t ubrr);
 
-void setup_stdio(int uart_num, FILE *stream);
-int __UART_transmit_stdout_num0(char data, FILE *stream);
-int __UART_transmit_stdout_num1(char data, FILE *stream);
-int __UART_receive_stdin_num0(FILE *stream);
-int __UART_receive_stdin_num1(FILE *stream);
+void UART0_setup_stdio(FILE *stream);
+void UART1_setup_stdio(FILE *stream);
+
+int __UART0_transmit_stdout(char data, FILE *stream);
+int __UART1_transmit_stdout(char data, FILE *stream);
+int __UART0_receive_stdin(FILE *stream);
+int __UART1_receive_stdin(FILE *stream);
+
+void __UART0_transmit(uint8_t data);
+void __UART1_transmit(uint8_t data);
+uint8_t __UART0_receive(void);
+uint8_t __UART1_receive(void);
+
+void UART0_flush(void);
+void UART1_flush(void);
 
 /*
 <connection>
