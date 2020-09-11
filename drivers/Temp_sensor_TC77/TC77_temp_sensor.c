a9835eaa89015d7943805a256ea5a21e973280f4
TPTplayer <potalgun0921@naver.com>
2020-09-11 16:52:37 +0900

    update: 2020.09.11

---
+++ drivers/Temp_sensor_TC77/TC77_temp_sensor.c
@@ -0,0 +1,66 @@
+﻿#include <avr/io.h>
+#include <stdint.h>
+
+#include "TC77_temp_sensor.h"
+
+void tc77_init(void){
+    _DDR_TC77 |= (_TC77_CS_PIN | _TC77_SCK_PIN);
+    _DDR_TC77 &= (~_TC77_DATA_PIN);
+    return;
+}
+
+void _tc77_cs_enable(void){
+    _PORT_TC77 &= (~_TC77_CS_PIN);
+    return;
+}
+
+void _tc77_cs_disable(void){
+    _PORT_TC77 |= _TC77_CS_PIN;
+    return;
+}
+
+uint16_t _tc77_get_rawdata(void){
+    uint16_t rawdata = 0x0000;
+    int cnt = 0;
+    
+    for(cnt = 0; cnt < 16; cnt++){
+        _TC77_SCK_HIGH;
+        rawdata <<= 1;
+        
+        if(_PIN_TC77 & _TC77_DATA_PIN){
+            rawdata |= 0x01;    
+        }
+        
+        _TC77_SCK_LOW;
+    }
+    
+    return rawdata;
+}
+
+float tc77_get_temp(void){
+    uint16_t rawdata = 0x0000;
+    float temp = 0;
+    
+    while(1){
+        _tc77_cs_enable();
+        rawdata = _tc77_get_rawdata();
+        _tc77_cs_disable();
+        
+        //rawdata = 0x0C87; //for checking TC77 (expected: 25 celcious)
+        
+        if(rawdata & _CONV_COMP_MASK){
+            break;
+        }
+    }    
+    
+    if(rawdata & _MASK_SIGN){
+        rawdata = (~(rawdata & _MASK_TEMP) >> 3) + 0x01;
+        temp = rawdata * _LSB * -1;
+    }
+    else{
+        rawdata = ((rawdata & _MASK_TEMP) >> 3);
+        temp = rawdata * _LSB;
+    }
+    
+    return temp;
+}
---
+++ drivers/Temp_sensor_TC77/TC77_temp_sensor.h
@@ -0,0 +1,38 @@
+﻿#ifndef TC77_TEMP_SENSOR_H_
+#define TC77_TEMP_SENSOR_H_
+
+#include <avr/io.h>
+#include <stdint.h>
+
+#define SET     (unsigned) 1
+#define CLEAR   (unsigned) 0
+
+#define _MASK_SIGN   0x1000
+#define _MASK_TEMP   0x7FF1
+#define _LSB         0.0625
+
+#define _CONV_COMP_MASK 0x0004 
+
+#define _TC77_CS    0
+#define _TC77_SCK   1
+#define _TC77_DATA  3
+
+#define _TC77_CS_PIN    (1 << _TC77_CS)
+#define _TC77_SCK_PIN   (1 << _TC77_SCK)
+#define _TC77_DATA_PIN  (1 << _TC77_DATA)
+
+#define _DDR_TC77   DDRB
+#define _PORT_TC77  PORTB
+#define _PIN_TC77   PINB
+
+#define _TC77_SCK_HIGH   _PORT_TC77 |= _TC77_SCK_PIN
+#define _TC77_SCK_LOW    _PORT_TC77 &= (~_TC77_SCK_PIN)
+
+void tc77_init(void);
+float tc77_get_temp(void);
+
+void _tc77_cs_enable(void);
+void _tc77_cs_disable(void);
+uint16_t _tc77_get_rawdata(void);
+
+#endif /* TC77_TEMP_SENSOR_H_ */
