03e62a25a5da65e3d748dc923397923de60a1f03
TPTplayer <potalgun0921@naver.com>
2020-09-17 16:30:19 +0900

    update: 2020.09.17

--- projects/KJ_playscale/KJ_playscale.c
+++ projects/KJ_playscale/KJ_playscale.c
@@ -1,10 +1,45 @@
 ﻿#include "KJ_playscale.h"
 
+static uint8_t scale_to_intager[B + 1];
+static uint8_t intager_to_scale[SCALE_PER_OCTAVE] = {C, CS, D, DS, E, F, FS, G, GS, A, AS, B};
+
+static uint8_t octave_to_intager[OCT8 + 1];
+static uint8_t intager_to_octave[OCTAVE_NUM] = {OCT1, OCT2, OCT3, OCT4, OCT5, OCT6, OCT7, OCT8};
+
+void init_KJ_playscale(void){
+    int oct_idx = 0, scale_idx = 0;
+    
+    for(oct_idx = 0; oct_idx < OCTAVE_NUM; oct_idx++){
+        octave_to_intager[intager_to_octave[oct_idx]] = oct_idx;
+    }
+    for(scale_idx = 0; scale_idx < SCALE_PER_OCTAVE; scale_idx++){
+        scale_to_intager[intager_to_scale[scale_idx]] = scale_idx;
+    }
+    
+    return;    
+}
+
+int scale_to_intagerf(uint8_t scale){
+    return scale_to_intager[scale];
+}
+
+uint8_t intager_to_scalef(int scale_num){
+    return intager_to_scale[scale_num];
+}
+
+int octave_to_intagerf(uint8_t octave){
+    return octave_to_intager[octave];
+}
+
+uint8_t intager_to_octavef(int octave_num){
+    return intager_to_octave[octave_num];
+}
+
 float calc_scalefreq(int octave, int scale){
     float freq = 0.0f;
     float numerator[SCALE_PER_OCTAVE] = {-9.0f, -8.0f, -7.0f, -6.0f, -5.0f, -4.0f, -3.0f, -1.0f, 0.0f, 1.0f, 2.0f};
     
-    freq = (A_OCT1_FREQ * pow(2.0f, (float)octave)) * pow(2.0f, numerator[scale] / (float)SCALE_PER_OCTAVE);
+    freq = (A_OCT1_FREQ * pow(2.0f, (float)octave)) * pow(2.0f, numerator[scale_to_intager[scale]] / (float)SCALE_PER_OCTAVE);
     return freq;
 }
 
@@ -16,3 +51,30 @@ float calc_clock(uint32_t clockspeed, float freq){
     return clock;
 }
 
+void get_all_scalefreq(float *p){
+    static float freqs[OCTAVE_NUM * SCALE_PER_OCTAVE] = {0.0f, };
+    int oct_idx = 0, scale_idx = 0;
+    
+    for(oct_idx = 0; oct_idx < OCTAVE_NUM; oct_idx++){
+        for(scale_idx = 0; scale_idx < SCALE_PER_OCTAVE; scale_idx++){
+            freqs[(oct_idx * SCALE_PER_OCTAVE) + scale_idx] = calc_scalefreq(intager_to_octave[oct_idx], intager_to_scale[scale_idx]);
+        }
+    }        
+    
+    p = freqs;
+    return;    
+}
+
+void get_all_required_clock(float *p, float *scalefreqs, uint32_t clockspeed){
+    static float clocks[OCTAVE_NUM * SCALE_PER_OCTAVE] = {0.0f, };
+    int oct_idx = 0, scale_idx = 0;
+    
+    for(oct_idx = 0; oct_idx < OCTAVE_NUM; oct_idx++){
+        for(scale_idx = 0; scale_idx < SCALE_PER_OCTAVE; scale_idx++){
+            clocks[(oct_idx * SCALE_PER_OCTAVE) + scale_idx] = calc_clock(clockspeed, scalefreqs[(oct_idx * SCALE_PER_OCTAVE) + scale_idx]);
+        }
+    }
+    
+    p = clocks;
+    return;    
+}
--- projects/KJ_playscale/KJ_playscale.h
+++ projects/KJ_playscale/KJ_playscale.h
@@ -4,11 +4,6 @@
 #include <math.h>
 #include <stdint.h>
 
-#ifndef F_CPU
-#warning "<KJ_playscale>: F_CPU is not defined for calculate operation."
-#define F_CPU (uint32_t)16000000
-#endif
-
 //definition octaves
 #define OCT1    0
 #define OCT2    1
@@ -37,8 +32,16 @@
 //standard frequency: octave4 scale A (440Hz)
 #define A_OCT1_FREQ         55
 #define SCALE_PER_OCTAVE    12
+#define OCTAVE_NUM          8
 
+void init_KJ_playscale(void);
+int scale_to_intagerf(uint8_t scale);
+uint8_t intager_to_scalef(int scale_num);
+int octave_to_intagerf(uint8_t octave);
+uint8_t intager_to_octavef(int octave_num);
 float calc_scalefreq(int octave, int scale);
 float calc_clock(uint32_t clockspeed, float freq);
+void get_all_scalefreq(float *p);
+void get_all_required_clock(float *p, float *scalefreqs, uint32_t clockspeed);
 
 #endif /* KJ_PLAYSCALE_H_ */
