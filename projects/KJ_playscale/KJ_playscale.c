e79534cd1bc1ed4d5cc824e60e8c68f3c22dfc7d
TPTplayer <potalgun0921@naver.com>
2020-09-17 15:30:17 +0900

    update: 2020.09.17

---
+++ projects/KJ_playscale/KJ_playscale.c
@@ -0,0 +1,18 @@
+﻿#include "KJ_playscale.h"
+
+float calc_scalefreq(int octave, int scale){
+    float freq = 0.0f;
+    float numerator[SCALE_PER_OCTAVE] = {-9.0f, -8.0f, -7.0f, -6.0f, -5.0f, -4.0f, -3.0f, -1.0f, 0.0f, 1.0f, 2.0f};
+    
+    freq = (A_OCT1_FREQ * pow(2.0f, (float)octave)) * pow(2.0f, numerator[scale] / (float)SCALE_PER_OCTAVE);
+    return freq;
+}
+
+float calc_clock(uint32_t clockspeed, float freq){
+    float clock = 0.0f;
+    float microsec_per_clock = (float)clockspeed / 1000000.0f;
+    
+    clock = microsec_per_clock * ((1.0f / freq / 2.0f) * 1000000.0f);
+    return clock;
+}
+
---
+++ projects/KJ_playscale/KJ_playscale.h
@@ -0,0 +1,44 @@
+﻿#ifndef KJ_PLAYSCALE_H_
+#define KJ_PLAYSCALE_H_
+
+#include <math.h>
+#include <stdint.h>
+
+#ifndef F_CPU
+#warning "<KJ_playscale>: F_CPU is not defined for calculate operation."
+#define F_CPU (uint32_t)16000000
+#endif
+
+//definition octaves
+#define OCT1    0
+#define OCT2    1
+#define OCT3    2
+#define OCT4    3
+#define OCT5    4
+#define OCT6    5
+#define OCT7    6
+#define OCT8    7
+
+//definition scales
+#define C   0
+#define CS  1
+#define D   2
+#define DS  3
+#define E   4
+#define F   5
+#define FS  6
+#define G   7
+#define GS  8
+#define A   9
+#define AS  10
+#define B   11  
+
+//octave1 scale A frequency
+//standard frequency: octave4 scale A (440Hz)
+#define A_OCT1_FREQ         55
+#define SCALE_PER_OCTAVE    12
+
+float calc_scalefreq(int octave, int scale);
+float calc_clock(uint32_t clockspeed, float freq);
+
+#endif /* KJ_PLAYSCALE_H_ */
