119c434bcdd04bca56d330155eac862672d2fd05
TPTplayer <potalgun0921@naver.com>
2020-09-18 12:53:23 +0900

    update: 2020.09.18

--- projects/KJ_playscale/KJ_playscale.c
+++ projects/KJ_playscale/KJ_playscale.c
@@ -1,45 +1,24 @@
 ﻿#include "KJ_playscale.h"
 
-static uint8_t scale_to_intager[B + 1];
-static uint8_t intager_to_scale[SCALE_PER_OCTAVE] = {C, CS, D, DS, E, F, FS, G, GS, A, AS, B};
-
-static uint8_t octave_to_intager[OCT8 + 1];
-static uint8_t intager_to_octave[OCTAVE_NUM] = {OCT1, OCT2, OCT3, OCT4, OCT5, OCT6, OCT7, OCT8};
-
-void init_KJ_playscale(void){
-    int oct_idx = 0, scale_idx = 0;
+octaveclock_t get_clockrange(uint32_t clockspeed, uint8_t octavename){
+    int cnt = 0;
+    float freq;
+    octaveclock_t clocks;
     
-    for(oct_idx = 0; oct_idx < OCTAVE_NUM; oct_idx++){
-        octave_to_intager[intager_to_octave[oct_idx]] = oct_idx;
-    }
-    for(scale_idx = 0; scale_idx < SCALE_PER_OCTAVE; scale_idx++){
-        scale_to_intager[intager_to_scale[scale_idx]] = scale_idx;
+    for(cnt = 0; cnt < SCALE_PER_OCTAVE; cnt++){
+        freq = calc_scalefreq(octavename, GET_SCALENAME(cnt));
+        clocks.clockspeed = clockspeed;
+        clocks.clock[cnt] = calc_clock(octavename, freq);
     }
     
-    return;    
+    return clocks;
 }
 
-int scale_to_intagerf(uint8_t scale){
-    return scale_to_intager[scale];
-}
-
-uint8_t intager_to_scalef(int scale_num){
-    return intager_to_scale[scale_num];
-}
-
-int octave_to_intagerf(uint8_t octave){
-    return octave_to_intager[octave];
-}
-
-uint8_t intager_to_octavef(int octave_num){
-    return intager_to_octave[octave_num];
-}
-
-float calc_scalefreq(int octave, int scale){
+float calc_scalefreq(int octavename, int scalename){
     float freq = 0.0f;
-    float numerator[SCALE_PER_OCTAVE] = {-9.0f, -8.0f, -7.0f, -6.0f, -5.0f, -4.0f, -3.0f, -1.0f, 0.0f, 1.0f, 2.0f};
+    float numerator[SCALE_PER_OCTAVE] = {-9.0f, -8.0f, -7.0f, -6.0f, -5.0f, -4.0f, -3.0f, -2.0f, -1.0f, 0.0f, 1.0f, 2.0f};
     
-    freq = (A_OCT1_FREQ * pow(2.0f, (float)octave)) * pow(2.0f, numerator[scale_to_intager[scale]] / (float)SCALE_PER_OCTAVE);
+    freq = (A_OCT1_FREQ * pow(2.0f, (float)GET_OCTNUM(octavename))) * pow(2.0f, numerator[GET_SCALENUM(scalename)] / (float)SCALE_PER_OCTAVE);
     return freq;
 }
 
@@ -49,32 +28,4 @@ float calc_clock(uint32_t clockspeed, float freq){
     
     clock = microsec_per_clock * ((1.0f / freq / 2.0f) * 1000000.0f);
     return clock;
-}
-
-void get_all_scalefreq(float *p){
-    static float freqs[OCTAVE_NUM * SCALE_PER_OCTAVE] = {0.0f, };
-    int oct_idx = 0, scale_idx = 0;
-    
-    for(oct_idx = 0; oct_idx < OCTAVE_NUM; oct_idx++){
-        for(scale_idx = 0; scale_idx < SCALE_PER_OCTAVE; scale_idx++){
-            freqs[(oct_idx * SCALE_PER_OCTAVE) + scale_idx] = calc_scalefreq(intager_to_octave[oct_idx], intager_to_scale[scale_idx]);
-        }
-    }        
-    
-    p = freqs;
-    return;    
-}
-
-void get_all_required_clock(float *p, float *scalefreqs, uint32_t clockspeed){
-    static float clocks[OCTAVE_NUM * SCALE_PER_OCTAVE] = {0.0f, };
-    int oct_idx = 0, scale_idx = 0;
-    
-    for(oct_idx = 0; oct_idx < OCTAVE_NUM; oct_idx++){
-        for(scale_idx = 0; scale_idx < SCALE_PER_OCTAVE; scale_idx++){
-            clocks[(oct_idx * SCALE_PER_OCTAVE) + scale_idx] = calc_clock(clockspeed, scalefreqs[(oct_idx * SCALE_PER_OCTAVE) + scale_idx]);
-        }
-    }
-    
-    p = clocks;
-    return;    
 }
--- projects/KJ_playscale/KJ_playscale.h
+++ projects/KJ_playscale/KJ_playscale.h
@@ -5,28 +5,35 @@
 #include <stdint.h>
 
 //definition octaves
-#define OCT1    0
-#define OCT2    1
-#define OCT3    2
-#define OCT4    3
-#define OCT5    4
-#define OCT6    5
-#define OCT7    6
-#define OCT8    7
+#define BVAL_OCT    0x04
+#define OCT1    0x00 + BVAL_OCT
+#define OCT2    0x01 + BVAL_OCT
+#define OCT3    0x02 + BVAL_OCT
+#define OCT4    0x03 + BVAL_OCT
+#define OCT5    0x04 + BVAL_OCT
+#define OCT6    0x05 + BVAL_OCT
+#define OCT7    0x06 + BVAL_OCT
+#define OCT8    0x07 + BVAL_OCT
 
 //definition scales
-#define C   0
-#define CS  1
-#define D   2
-#define DS  3
-#define E   4
-#define F   5
-#define FS  6
-#define G   7
-#define GS  8
-#define A   9
-#define AS  10
-#define B   11  
+#define BVAL_SCALE  0x05
+#define C   0x00 + BVAL_SCALE
+#define CS  0x01 + BVAL_SCALE
+#define D   0x02 + BVAL_SCALE
+#define DS  0x03 + BVAL_SCALE
+#define E   0x04 + BVAL_SCALE
+#define F   0x05 + BVAL_SCALE
+#define FS  0x06 + BVAL_SCALE
+#define G   0x07 + BVAL_SCALE
+#define GS  0x08 + BVAL_SCALE
+#define A   0x09 + BVAL_SCALE
+#define AS  0x0A + BVAL_SCALE
+#define B   0x0B + BVAL_SCALE
+
+#define GET_OCTNUM(_OCTAVENAME_)    (_OCTAVENAME_ - BVAL_OCT)
+#define GET_OCTNAME(_OCTNUM_)       (_OCTNUM_ - BVAL_OCT)
+#define GET_SCALENUM(_SCALENAME_)   (_SCALENAME_ + BVAL_SCALE)
+#define GET_SCALENAME(_SCALENUM_)   (_SCALENUM_ + BVAL_SCALE)
 
 //octave1 scale A frequency
 //standard frequency: octave4 scale A (440Hz)
@@ -34,14 +41,13 @@
 #define SCALE_PER_OCTAVE    12
 #define OCTAVE_NUM          8
 
-void init_KJ_playscale(void);
-int scale_to_intagerf(uint8_t scale);
-uint8_t intager_to_scalef(int scale_num);
-int octave_to_intagerf(uint8_t octave);
-uint8_t intager_to_octavef(int octave_num);
-float calc_scalefreq(int octave, int scale);
+typedef struct{
+    float clock[SCALE_PER_OCTAVE];
+    uint32_t clockspeed;
+}octaveclock_t;
+
+octaveclock_t get_clockrange(uint32_t clockspeed, uint8_t octavename);
+float calc_scalefreq(int octavename, int scalename);
 float calc_clock(uint32_t clockspeed, float freq);
-void get_all_scalefreq(float *p);
-void get_all_required_clock(float *p, float *scalefreqs, uint32_t clockspeed);
 
 #endif /* KJ_PLAYSCALE_H_ */
